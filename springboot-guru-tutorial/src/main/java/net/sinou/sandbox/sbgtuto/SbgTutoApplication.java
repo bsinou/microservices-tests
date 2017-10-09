9e1118c8fae069e561db9f68026cdf793197e00b
Bruno Sinou <bruno.sinou@gmail.com>
2017-10-09 15:25:38 +0200

    Create specific project for the springframework.guru tutorial

---
+++ springboot-guru-tutorial/src/main/java/net/sinou/sandbox/sbgtuto/SbgTutoApplication.java
@@ -0,0 +1,12 @@
+package net.sinou.sandbox.sbgtuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbgTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbgTutoApplication.class, args);
+	}
+}
